1bff260e24f155abc9c749d42632f03d8b3cd204
Sonivaish <149410045+Sonivaish@users.noreply.github.com>
2025-07-27 18:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,24 @@ def suggest_careers(user_input):
        careers = json.load(f)
 
     scores = []
-    for career in carers:
+    for career in careers:
         match_count = sum(1 for kw in career['keywords'] if kw in user_input.lower())
         scores.append((career['career'], match_count))
 
-    scores.sort(key= lambda x: x[1], reverse=True)
+    scores.sort(key=lambda x: x[1], reverse=True)
     return[career for career, score in scores if score > 0][:3]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/result' , methods=['post'])
+@app.route('/result' , methods=['POST'])
 def result():
     interests = request.form['interests']
     strengths = request.form['strengths']
     workstyle = request.form['workstyle']
 
-    user_input = f"{interests} {strengths {workstyle}"
+    user_input = f"{interests} {strengths} {workstyle}"
     suggestions = suggest_careers(user_input)
 
     return render_template('result.html', suggestions=suggestions)
